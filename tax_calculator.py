9397a3595eff9e7c9f0cc19a0ff1f9bf6c85bbcb
emanuelg84 <65663811+emanuelg84@users.noreply.github.com>
2020-06-27 08:43:19 -0400

    Update tax_calculator.py

--- tax_calculator.py
+++ tax_calculator.py
@@ -3,8 +3,32 @@ import matplotlib.pyplot as plt
 import os
 
 Income = input("insert your annual salary: ")
-state = input("insert the state you would like to analyse: ")
-status = input("insert status (for single enter "s" for marreid enter "m"): ")
+state = input("insert the state you would like to analyse (in capital letters): ")
+status = input("insert status (for single enter the lettter s for marreid enter the letter m): ")
 
-State_tax = pandas.read_csv(f'data/State_tax_{status}.csv')
-Federal_tax = pandas.read_csv(f'data/Federal_tax_{status}.csv')
+State_tax = pandas.read_csv(f'data/State_tax_{status}.csv', names = ['State','Income',"Tax rate"])
+Federal_tax = pandas.read_csv(f'data/Federal_tax_{status}.csv',names = ['Income level',"Tax rate"])
+
+agg_income = []
+
+for i, row in State_tax.iterrows():
+    if (row['State']) == state:
+        d = {"bracket":row['Income'],"tax_rate":row['Tax rate']}
+        agg_income.append(d)
+
+
+float(agg_income_int = 0)
+
+for i in agg_income:
+    #print(type(i['tax_rate']))
+    b = ((float(i['bracket']) * float(i['tax_rate']))
+    agg_income_int = agg_income_int + b
+    #agg_income_int=agg_income_int+i['bracket']
+    # print(str(int((i['bracket'])) * int(i['tax_rate'])))
+    #agg_income_int = agg_income_int + (int(i['bracket']) * int(i['tax_rate']))
+
+print(str(agg_income_int))
+
+#for i in Federal_tax:  
+    #print(Federal_tax["i"])   
+ 
